7fdd24ccd14fa2396a78933ee9dcc4bb588580c6
Kienan Adams <kra8ff@virginia.edu>
2016-01-14 18:13:12 -0500

    Refactor js a bit

--- app.js
+++ app.js
@@ -1,58 +1,55 @@
-var apiKey = 'E8JrpEgVCBglciJxgutsr6MQ51NGo5Vi';
-var userID = 'RebeccaEileenAdams';
-
-(function() {
-	var behanceUserAPI = 'http://www.behance.net/v2/users/' + userID + '?callback=?&api_key=' + apiKey;
-
-	function setUserTemplate() {
-		var userData = JSON.parse(sessionStorage.getItem('behanceUser')),
-			getTemplate = $('#profile-template').html(),
-			template = Handlebars.compile(getTemplate),
-			result = template(userData);
-		$('#header').html(result);
-	};
-
-	if (sessionStorage.getItem('behanceUser')) {
-		setUserTemplate();
-	} else {
-		$.getJSON(behanceUserAPI, function(user) {
-			var data = JSON.stringify(user);
-			sessionStorage.setItem('behanceUser', data);
-			setUserTemplate();
-		});
-	};
-
-})();
-
-(function() {
-	var perPage = 12;
-	var behanceProjectAPI = 'http://www.behance.net/v2/users/' + userID + '/projects?callback=?&api_key=' + apiKey + '&per_page=' + perPage;
-
-	function setPortfolioTemplate() {
-		var projectData = JSON.parse(sessionStorage.getItem('behanceProject')),
-			getTemplate = $('#portfolio-template').html(),
-			template = Handlebars.compile(getTemplate),
-			result = template(projectData);
-		$('#portfolio').html(result);
-	};
-
-	if (sessionStorage.getItem('behanceProject')) {
-		setPortfolioTemplate();
-	} else {
-		$.getJSON(behanceProjectAPI, function(project) {
-			var data = JSON.stringify(project);
-			sessionStorage.setItem('behanceProject', data);
-			setPortfolioTemplate();
-		});
-	};
-})();
-
 var ready;
 
 ready = function() {
 
 	var apiKey = 'E8JrpEgVCBglciJxgutsr6MQ51NGo5Vi';
 	var userID = 'RebeccaEileenAdams';
+
+	(function() {
+		var behanceUserAPI = 'http://www.behance.net/v2/users/' + userID + '?callback=?&api_key=' + apiKey;
+
+		function setUserTemplate() {
+			var userData = JSON.parse(sessionStorage.getItem('behanceUser')),
+				getTemplate = $('#profile-template').html(),
+				template = Handlebars.compile(getTemplate),
+				result = template(userData);
+			$('#header').html(result);
+		};
+
+		if (sessionStorage.getItem('behanceUser')) {
+			setUserTemplate();
+		} else {
+			$.getJSON(behanceUserAPI, function(user) {
+				var data = JSON.stringify(user);
+				sessionStorage.setItem('behanceUser', data);
+				setUserTemplate();
+			});
+		};
+
+	})();
+
+	(function() {
+		var perPage = 12;
+		var behanceProjectAPI = 'http://www.behance.net/v2/users/' + userID + '/projects?callback=?&api_key=' + apiKey + '&per_page=' + perPage;
+
+		function setPortfolioTemplate() {
+			var projectData = JSON.parse(sessionStorage.getItem('behanceProject')),
+				getTemplate = $('#portfolio-template').html(),
+				template = Handlebars.compile(getTemplate),
+				result = template(projectData);
+			$('#portfolio').html(result);
+		};
+
+		if (sessionStorage.getItem('behanceProject')) {
+			setPortfolioTemplate();
+		} else {
+			$.getJSON(behanceProjectAPI, function(project) {
+				var data = JSON.stringify(project);
+				sessionStorage.setItem('behanceProject', data);
+				setPortfolioTemplate();
+			});
+		};
+	})();
 
 	$(window).on("scroll touchmove", function(e) {
 		var distanceY = window.pageYOffset || document.documentElement.scrollTop,
